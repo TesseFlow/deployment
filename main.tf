3b0ed55c8db009ee61b04e875ae049eb2e2bee79
whereyourspace <whereyourspace@gmail.com>
2025-04-07 23:41:05 +0300

    Hotfix devcontainer and terraform Fixed issues with devcontainer and duplicated required_providers

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_version = "~>1.11.0"
-  required_providers {
-    vault = {
-      source  = "hashicorp/vault"
-      version = "4.7.0"
-    }
-    proxmox = {
-      source = "telmate/proxmox"
-      version = "3.0.1-rc6"
-    }
-  }
-}
 provider "vault" {
   # Use the folowwing environment variables for confiration
   # VAULT_ADDR -> vault server address
